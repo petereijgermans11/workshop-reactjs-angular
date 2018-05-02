ca7093b4a5e58e8c7073995efedcda61db5dc42d
petereijgermans11 <p.eijgermans11@gmail.com>
2018-05-02 07:29:31 +0200

    mock-books

--- workshop-reactjs-angular/angular/src/app/mock-books.ts
+++ workshop-reactjs-angular/angular/src/app/mock-books.ts
@@ -1,14 +1,14 @@
 import { Book } from './book';
 
 export const BOOKS: Book[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Using React and Redux' },
+  { id: 12, name: 'Angular in Action' },
+  { id: 13, name: 'NativeScript in Action' },
+  { id: 14, name: 'Vuejs in Action' },
+  { id: 15, name: 'Introduction to IOT' },
+  { id: 16, name: 'Learning React Native' },
+  { id: 17, name: 'Functional programming' },
+  { id: 18, name: 'JavaScript the good parts' },
+  { id: 19, name: 'JavaScript Patterns' },
+  { id: 20, name: 'Jasmine JavaScript testing' }
 ];
