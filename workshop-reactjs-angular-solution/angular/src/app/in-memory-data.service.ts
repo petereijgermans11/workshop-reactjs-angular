7e34db636e501d6b849fec0d53c7035d4a2253ad
petereijgermans11 <p.eijgermans11@gmail.com>
2018-06-05 14:55:47 +0200

    in memory data

--- workshop-reactjs-angular-solution/angular/src/app/in-memory-data.service.ts
+++ workshop-reactjs-angular-solution/angular/src/app/in-memory-data.service.ts
@@ -3,16 +3,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const books = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Using React and Redux' },
+      { id: 12, name: 'Angular in Action' },
+      { id: 13, name: 'NativeScript in Action' },
+      { id: 14, name: 'Vuejs in Action' },
+      { id: 15, name: 'Introduction to IOT' },
+      { id: 16, name: 'Learning React Native' },
+      { id: 17, name: 'Functional programming' },
+      { id: 18, name: 'JavaScript the good parts' },
+      { id: 19, name: 'JavaScript Patterns' },
+      { id: 20, name: 'Jasmine JavaScript testing' }
     ];
     return {books};
   }
